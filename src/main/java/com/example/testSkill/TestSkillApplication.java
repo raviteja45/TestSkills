fdce0c380a16077b8bbbe4da9ecf977eb0160b88
raviteja45 <raviteja.gitam@gmail.com>
2019-11-10 22:37:57 +0530

    Added the comment.

--- src/main/java/com/example/testSkill/TestSkillApplication.java
+++ src/main/java/com/example/testSkill/TestSkillApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestSkillApplication {
 
+	/**Adding comments*/
 	public static void main(String[] args) {
 		SpringApplication.run(TestSkillApplication.class, args);
 	}
