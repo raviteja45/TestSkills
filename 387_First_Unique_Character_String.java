56c3a2e6179530995107b480f8ed8adfdae3fb46
raviteja45 <raviteja.gitam@gmail.com>
2021-04-16 16:05:20 -0500

    Create 387_First_Unique_Character_String.java

---
+++ 387_First_Unique_Character_String.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int firstUniqChar(String s) {
+       /** Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+           map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+        return -1;*/
+        for(int i=0;i<s.length();i++){
+            if(s.indexOf(s.charAt(i))==s.lastIndexOf(s.charAt(i))){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+/**
+Given a string s, return the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+*/
