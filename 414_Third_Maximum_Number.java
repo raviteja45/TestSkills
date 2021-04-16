41d8a2194f05fc672a901d6c922a30dd66383b7d
raviteja45 <raviteja.gitam@gmail.com>
2021-04-16 09:48:18 -0500

    Create 414_Third_Maximum_Number.java

---
+++ 414_Third_Maximum_Number.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Set<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        List<Integer> li = new ArrayList<>();
+        li.addAll(set);
+        if(li.size()>=3){
+            return li.get(2);
+        }else{
+            return li.get(0);
+        }
+    }
+}
+
+
+/**
+
+Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation: The third maximum is 1.
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+
+*/
