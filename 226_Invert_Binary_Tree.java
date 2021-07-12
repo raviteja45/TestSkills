a080acf1caa1134feb501dc1b462c5eb45c5009b
raviteja45 <raviteja.gitam@gmail.com>
2021-07-12 15:32:12 -0500

    Create 226_Invert_Binary_Tree.java

---
+++ 226_Invert_Binary_Tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        TreeNode m = root;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(m);
+        while(q.size()>0){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                TreeNode t = q.poll();
+                TreeNode temp = t.left;
+                t.left = t.right;
+                t.right = temp;
+                if(t.left!=null){
+                    q.add(t.left);
+                }
+                if(t.right!=null){
+                    q.add(t.right);
+                }
+            }
+        }
+        return m;
+    }
+}
+
+
+/**
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+
+*/
