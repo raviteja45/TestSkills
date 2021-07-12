0b50f2a75d4e9baed93e9efa73a2b3243cf0bca2
raviteja45 <raviteja.gitam@gmail.com>
2021-07-12 10:41:25 -0500

    Create 230_Kth_Smallest_Element_in_BST.java

---
+++ 230_Kth_Smallest_Element_in_BST.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        Set<Integer> s = new TreeSet<>();
+        while(!q.isEmpty()){
+           int n= q.size();
+            for(int i=0;i<n;i++){
+                TreeNode t = q.poll();
+                s.add(t.val);
+                if(t.left!=null){
+                    q.add(t.left);
+                }
+                if(t.right!=null){
+                    q.add(t.right);
+                }
+            }
+        }
+        int n = 0;
+        if(!s.isEmpty()){
+            for(Integer l:s){
+                if(n==k-1){
+                    return l;
+                }
+                n++;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+/**
+Given the root of a binary search tree, and an integer k, return the kth (1-indexed) smallest element in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+
+
+
+
+*/
