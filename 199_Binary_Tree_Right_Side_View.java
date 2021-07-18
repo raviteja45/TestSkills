981560b980404d98955d3b7e1cd443cbc8fc886a
raviteja45 <raviteja.gitam@gmail.com>
2021-07-18 11:22:55 -0500

    Create 199_Binary_Tree_Right_Side_View.java

---
+++ 199_Binary_Tree_Right_Side_View.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> li = new ArrayList<>();
+        if(root==null){
+            return li;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                TreeNode t = q.poll();
+                if(i==0){
+                    li.add(t.val);
+                }
+                if(t.right!=null){
+                    q.add(t.right);
+                }
+                if(t.left!=null){
+                    q.add(t.left);
+                }
+            }
+        }
+        return li;
+    }
+}
+
+
+
+/**
+
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+Example 2:
+
+Input: root = [1,null,3]
+Output: [1,3]
+Example 3:
+
+Input: root = []
+Output: []
+
+
+*/
