7b2312419f8f8bb0887615f7bd32acaed28ca1c3
raviteja45 <raviteja.gitam@gmail.com>
2021-07-28 14:41:58 -0500

    Create 94_Binary_Tree_Inorder_Traversal.java

---
+++ 94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+       Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        while(root!=null||!stack.isEmpty()){
+            while(root!=null){
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode t = stack.pop();
+            result.add(t.val);
+            root = t.right;
+            
+        }
+        return result;
+    }
+}
+
+
+/**
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [2,1]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+
+*/
