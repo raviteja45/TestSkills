fbb89f6284167e01c041f683d317921eb8691f41
raviteja45 <raviteja.gitam@gmail.com>
2021-08-02 10:08:34 -0500

    Create 78_Subsets.java

---
+++ 78_Subsets.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+       List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int num:nums){
+            int n = result.size();
+            for(int i=0;i<n;i++){
+                List<Integer>list = new ArrayList<>(result.get(i));
+                list.add(num);
+                result.add(list);
+                
+            }
+        }
+        return result;
+    }
+}
+
+
+/**
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+*/
