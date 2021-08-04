a94287fbba81522adef12aded7e15ebdd6b78f04
raviteja45 <raviteja.gitam@gmail.com>
2021-08-04 11:12:27 -0500

    Update 53_Maximum_Subarray.java

--- 53_Maximum_Subarray.java
+++ 53_Maximum_Subarray.java
@@ -1,12 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
-        int temp=0;
+        int temp = 0;
         for(int i=0;i<nums.length;i++){
-             temp = temp+nums[i];
-            if(max<temp){
-                max = temp;
-            }
+            temp = temp+nums[i];
+            max = Math.max(max,temp);
             if(temp<0){
                 temp = 0;
             }
