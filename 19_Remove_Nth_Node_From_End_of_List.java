2bdfd06268b67d349326aec83342b5f4828b1339
raviteja45 <raviteja.gitam@gmail.com>
2021-08-30 12:47:05 -0500

    Create 19_Remove_Nth_Node_From_End_of_List.java

---
+++ 19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode k = temp;
+        int count = 0;
+        while(temp!=null){
+            count = count+1;
+            temp = temp.next;
+            
+        }
+        count = count - n;
+        head = k;
+        while(count>1){
+            count = count-1;
+            head = head.next;
+        }
+        head.next = head.next.next;
+        return k.next;
+    }
+}
+
+
+/**
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+*/
