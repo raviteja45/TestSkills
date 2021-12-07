50272604682997679ecdb8ff5d59aecf0accf2e4
raviteja45 <raviteja.gitam@gmail.com>
2021-12-07 16:37:06 -0600

    Update 518_Coin_Change_2.java

--- 518_Coin_Change_2.java
+++ 518_Coin_Change_2.java
@@ -3,10 +3,8 @@ class Solution {
         int[] dp = new int[amount+1];
         dp[0] = 1;
         for(int coin:coins){
-            for(int j=0;j<=amount;j++){
-                if(j>=coin){
+            for(int j=coin;j<=amount;j++){
                     dp[j] = dp[j]+dp[j-coin];
-                }
             }
         }
         return dp[amount];
