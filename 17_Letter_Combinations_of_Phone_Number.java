76a1bd92f2cc71a727559f51f5cddefebb6e158c
raviteja45 <raviteja.gitam@gmail.com>
2021-12-20 12:34:10 -0600

    Update 17_Letter_Combinations_of_Phone_Number.java

--- 17_Letter_Combinations_of_Phone_Number.java
+++ 17_Letter_Combinations_of_Phone_Number.java
@@ -30,6 +30,42 @@ class Solution {
     }
 }
 
+----------- V2 version--------------
+	
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if(digits.isEmpty()){
+            return new ArrayList<>();
+        }
+       Map<Character,String> map = new HashMap<>();
+        map.put('2', "abc");
+	    map.put('3', "def");
+	    map.put('4', "ghi");
+	    map.put('5', "jkl");
+	    map.put('6', "mno");
+	    map.put('7', "pqrs");
+	    map.put('8', "tuv");
+	    map.put('9', "wxyz");
+        StringBuilder sb = new StringBuilder();
+        backTracking(digits,map,res,0,sb);
+        return res;
+    }
+    public void backTracking(String digits,Map<Character,String>map,List<String> res, int index, StringBuilder sb){
+        if(sb.length()==digits.length()){
+            res.add(sb.toString());
+        }else{
+            for(int i=index;i<digits.length();i++){
+                String s = map.get(digits.charAt(i));
+                for(int k = 0;k<s.length();k++){
+                    sb.append(s.charAt(k));
+                    backTracking(digits,map,res,i+1,sb);
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+        }
+    }
+}
 
 /**
 Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
