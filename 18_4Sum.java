dfd29433f19e1505753a4f4bda1482c25692b0e2
raviteja45 <raviteja.gitam@gmail.com>
2021-12-28 13:30:45 -0600

    Create 18_4Sum.java

---
+++ 18_4Sum.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> temp_res = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length-1;j++){
+                int low = j+1;
+                int high = nums.length-1;
+                while(low<high){
+                    int new_target = nums[i]+nums[low]+nums[high]+nums[j];
+                    if(target == new_target){
+                        temp_res.add(Arrays.asList(nums[i],nums[low],nums[high],nums[j]));
+                        low++;
+                        high--;
+                    }else if(new_target>target){
+                        high--;
+                    }else{
+                        low++;
+                    }
+                }
+            }
+        }
+        result.addAll(temp_res);
+        return result;
+    }
+}
+
+/**
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+*/
