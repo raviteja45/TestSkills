e2ff0f5e89c0655bf9996d499ff459c7d859230b
raviteja45 <raviteja.gitam@gmail.com>
2022-04-10 15:57:32 -0500

    Create Node_customHashMap.java

---
+++ src/Node_customHashMap.java
@@ -0,0 +1,34 @@
+package com.home.hashing;
+
+public class Node_customHashMap {
+	
+	private int value;
+	
+	private int key;
+	
+	private Node_customHashMap next;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public Node_customHashMap getNext() {
+		return next;
+	}
+
+	public void setNext(Node_customHashMap next) {
+		this.next = next;
+	}
+}
