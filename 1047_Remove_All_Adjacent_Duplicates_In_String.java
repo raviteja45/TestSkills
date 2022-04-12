6695468b82cd8da3d229a9025efc651d3eb14b6a
raviteja45 <raviteja.gitam@gmail.com>
2022-04-12 14:24:31 -0500

    Create 1047_Remove_All_Adjacent_Duplicates_In_String.java

---
+++ 1047_Remove_All_Adjacent_Duplicates_In_String.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty()&&stack.peek()==s.charAt(i)){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
+            }
+        }
+        if(!stack.isEmpty()){
+           for(char c: stack){
+               sb.append(c);
+           }
+        }
+        return sb.toString();
+    }
+}
+
+/**
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+*/
