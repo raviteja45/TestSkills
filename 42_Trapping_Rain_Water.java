108e683bf60f105317f1504438b62c06f4ed952d
raviteja45 <raviteja.gitam@gmail.com>
2022-06-14 13:38:35 -0500

    Create 42_Trapping_Rain_Water.java

---
+++ 42_Trapping_Rain_Water.java
@@ -0,0 +1,20 @@
+class Solution { // Same as histogram rectangle
+    public int trap(int[] height) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int  maxWater = 0;
+        for(int i=0;i<=height.length;i++){
+            int current = (i==height.length)?0:height[i];
+            while(!stack.isEmpty()&&current>height[stack.peek()]){ // here its reverse of histogram <, >
+                int height1 = height[stack.pop()];
+                if(stack.isEmpty()){
+                    break;
+                }
+                int bredth  = i-stack.peek()-1;//right-left-1
+                int water_filled = bredth*(Math.min(height[stack.peek()],current)-height1); // min (left, right)
+                maxWater = maxWater+water_filled;
+            }
+            stack.push(i);
+        }
+        return maxWater;
+    }
+}
