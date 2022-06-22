7b69ba6552a7b984df26f6d5e4746d130678b95c
raviteja45 <raviteja.gitam@gmail.com>
2022-06-22 11:27:18 -0500

    Create 1381_Design_a_Stack_With_Increment_Operation.java

---
+++ 1381_Design_a_Stack_With_Increment_Operation.java
@@ -0,0 +1,70 @@
+class CustomStack {
+
+    int[] arr = null;
+    int count = 0;
+    public CustomStack(int maxSize) {
+        arr = new int[maxSize];
+    }
+    
+    public void push(int x) {
+        if(arr.length>count){
+            arr[count] = x;
+            count++;
+        }
+        
+    }
+    
+    public int pop() {
+        if((count-1)>=0){
+            count = count-1;
+        return arr[count];
+        }
+        return -1;
+        
+    }
+    
+    public void increment(int k, int val) {
+        int temp = k>arr.length?arr.length:k;
+       for(int i=0;i<temp;i++){
+           arr[i] = arr[i]+val;
+       }
+        
+    }
+}
+
+
+
+/**
+Design a stack which supports the following operations.
+
+Implement the CustomStack class:
+
+CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack or do nothing if the stack reached the maxSize.
+void push(int x) Adds x to the top of the stack if the stack hasn't reached the maxSize.
+int pop() Pops and returns the top of stack or -1 if the stack is empty.
+void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, just increment all the elements in the stack.
+ 
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack customStack = new CustomStack(3); // Stack is Empty []
+customStack.push(1);                          // stack becomes [1]
+customStack.push(2);                          // stack becomes [1, 2]
+customStack.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+customStack.push(2);                          // stack becomes [1, 2]
+customStack.push(3);                          // stack becomes [1, 2, 3]
+customStack.push(4);                          // stack still [1, 2, 3], Don't add another elements as size is 4
+customStack.increment(5, 100);                // stack becomes [101, 102, 103]
+customStack.increment(2, 100);                // stack becomes [201, 202, 103]
+customStack.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+customStack.pop();                            // return 202 --> Return top of the stack 102, stack becomes [201]
+customStack.pop();                            // return 201 --> Return top of the stack 101, stack becomes []
+customStack.pop();                            // return -1 --> Stack is empty return -1.
+
+*/
