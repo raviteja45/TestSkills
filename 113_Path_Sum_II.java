cf891d8d7a144db3db9360dd1072622c0152940e
raviteja45 <raviteja.gitam@gmail.com>
2022-06-30 12:10:40 -0500

    Create 113_Path_Sum_II.java

---
+++ 113_Path_Sum_II.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        backTracking(root,res,targetSum,new ArrayList<>(),0);
+        return res;
+    }
+    
+    public void backTracking(TreeNode root, List<List<Integer>> res, int targetSum, List<Integer> temp,int curSum){
+        if(root==null){
+            return;
+        }
+        curSum = curSum+root.val;
+        temp.add(root.val);
+        if(curSum==targetSum&&root.left==null&&root.right==null){
+            res.add(new ArrayList<>(temp));
+        }
+            backTracking(root.left,res,targetSum,temp,curSum);
+            backTracking(root.right,res,targetSum,temp,curSum);
+        
+        temp.remove(temp.size()-1);
+        
+    }
+}
+
+/**
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+*/
