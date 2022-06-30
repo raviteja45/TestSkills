b8be59d58769431d6177ce26e31ea08633551f62
raviteja45 <raviteja.gitam@gmail.com>
2022-06-30 11:50:18 -0500

    Update 31_Next_Permutation.java

--- 31_Next_Permutation.java
+++ 31_Next_Permutation.java
@@ -34,6 +34,17 @@ class Solution {
     }
 }
 
+/**
+We want to find a larger number, but not too large - just the next larger one =>
+We want to avoid chaning the number in the left - it will increase the number too much =>
+Example: 4325413 -> we can only change the last two numbers and have 432531
+What if it was 432531 in the first place? 31 cannot be increased.
+Lets try 531 - still no
+2531 - this can be incrased - the smallest number that can be used to incrase the 2 is 3. so for now we have 3521.
+Next we want to minimize 3521 - thats easier - just sort the numbers to the right of 3 - 3125. So the unswer is 4323125 
+*/
+    
+    
 
 /**
 A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
