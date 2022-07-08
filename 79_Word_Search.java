3c0b7da10a3c7c4fd71499fe9017c5f40bfd3300
raviteja45 <raviteja.gitam@gmail.com>
2022-07-08 14:03:39 -0500

    Update 79_Word_Search.java

--- 79_Word_Search.java
+++ 79_Word_Search.java
@@ -33,6 +33,39 @@ class Solution {
     }
 }
 
+
+---------------------V2-----------------------
+    
+    class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(search(board,i,j,word,0,new boolean[board.length][board[0].length])){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public boolean search(char[][] board,int i,int j, String word,int index,boolean[][] visited){
+        if(index == word.length()){
+            return true;
+        }
+        if(i<0||j<0||i>=board.length||j>=board[0].length||board[i][j]!=word.charAt(index)|| visited[i][j]){
+            return false;
+        }
+        visited[i][j] = true;
+        boolean res = search(board,i+1,j,word,index+1,visited)||
+                      search(board,i-1,j,word,index+1,visited)||
+                      search(board,i,j+1,word,index+1,visited)||
+                      search(board,i,j-1,word,index+1,visited);
+        visited[i][j] = false;
+        return res;
+        
+    }
+}
 /**
 Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
