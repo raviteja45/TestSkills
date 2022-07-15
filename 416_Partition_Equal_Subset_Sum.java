a008dfe7f9eb0551657fcebcaadccf53b6a4a3cb
raviteja45 <raviteja.gitam@gmail.com>
2022-07-15 16:49:15 -0500

    Update 416_Partition_Equal_Subset_Sum.java

--- 416_Partition_Equal_Subset_Sum.java
+++ 416_Partition_Equal_Subset_Sum.java
@@ -53,7 +53,39 @@ class Solution {
     }
 }
 
-
+---------------------------------------v2 version ----------------------------------------------
+    
+    class Solution {
+    public boolean canPartition(int[] nums) { // Its similar to coin change 1 and 2
+        int sum = 0;
+        for(int num:nums){
+            sum= sum+num;
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        sum = sum/2; // This is the target sum. Its like coin change-2, Knapsack problem.
+        boolean[][] dp = new boolean[nums.length+1][sum+1];// Given nums is rows
+                                                           // from 0 to sum+1 is columns.
+                                                           // Rows will always be given   
+                                                           //array(nums)
+        for(int i=0;i<=nums.length;i++){
+            dp[i][0] = true; // All rows with 0th column is true.
+        }
+        
+        for(int i=1;i<=nums.length;i++){
+            for(int j=1;j<=sum;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j>=nums[i-1]){
+                    dp[i][j] = dp[i][j]||dp[i-1][j-nums[i-1]];
+ //we CANNOT reuse an element while it can be reused in Coin change-2. That is why we have dp[i-1][j-nums[i-1]] in this problem and dp[i][j-coins[i-1]] in LT518
+                }
+                
+            }
+        }
+        return dp[nums.length][sum];
+    }
+}
 
 /**
 Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
