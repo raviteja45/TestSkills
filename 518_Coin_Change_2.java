715523293963ad35ee108b22296289f3ae85ec54
raviteja45 <raviteja.gitam@gmail.com>
2022-07-15 17:55:17 -0500

    Update 518_Coin_Change_2.java

--- 518_Coin_Change_2.java
+++ 518_Coin_Change_2.java
@@ -39,6 +39,28 @@ class Solution {
     }
 }
 
+--------------------------------V3 version ----------------------------
+    
+    class Solution {// Same as Partition Equal Subset Sum, Coin change 1. It uses Knapsack problem
+    public int change(int amount, int[] coins) {
+      int[][] dp = new int[coins.length+1][amount+1]; // Rows will always be given array(coins)
+        for(int i=0;i<=coins.length;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j>=coins[i-1]){
+                    dp[i][j] = dp[i][j]+dp[i][j-coins[i-1]];
+// we CANNOT reuse an element while it can be reused in Coin change-2. 
+// That is why we have dp[i-1][j-nums[i-1]] in 416(partition equal subset sum) problem and dp[i][j-coins[i-1]] in this problem.
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
+
 /**
 You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
