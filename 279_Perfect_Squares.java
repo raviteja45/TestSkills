814cdbf05ef38a3f6850be08886dbb0fe2f4242d
raviteja45 <raviteja.gitam@gmail.com>
2022-07-21 09:20:53 -0500

    Update 279_Perfect_Squares.java

--- 279_Perfect_Squares.java
+++ 279_Perfect_Squares.java
@@ -19,6 +19,37 @@ class Solution {
     }
 }
 
+---------------------------v2 Version ---------------------------------
+    
+class Solution {
+    public int numSquares(int n) {//Its like coin change
+        int temp = 1;
+        List<Integer> li = new ArrayList<>();
+        while(temp*temp<=n){
+            li.add(temp*temp);
+            temp++;
+        }
+        int[][] dp = new int[li.size()+1][n+1];
+        for(int i=0;i<=li.size();i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j] = n+1;
+            }
+        }
+        for(int i=0;i<li.size();i++){
+            dp[i][0] = 0;
+        }
+        for(int i=1;i<=li.size();i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j>=li.get(i-1)){
+                    dp[i][j] = Math.min(dp[i][j],dp[i][j-li.get(i-1)]+1);
+                }
+            }
+        }
+        return dp[li.size()][n];
+    }
+}
+
 /**
 Given an integer n, return the least number of perfect square numbers that sum to n.
 
