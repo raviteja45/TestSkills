4d236d386d422f2b468f4cc4cce3dbe4413aeec1
raviteja45 <raviteja.gitam@gmail.com>
2022-08-01 17:40:20 -0500

    Update 114_Flatten_Binary_Tree_to_Linked_List.java

--- 114_Flatten_Binary_Tree_to_Linked_List.java
+++ 114_Flatten_Binary_Tree_to_Linked_List.java
@@ -20,3 +20,34 @@ class Solution {
         }
     }
 }
+
+-----------------------------V2 ------------------------------------
+    
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        stack.push(root);
+        while(!stack.isEmpty()){ // We are first creating pre-order traversal
+            TreeNode temp = stack.pop();
+            if(temp!=null)
+            q.add(temp); // Now elements are in pre-order traversal
+            if(temp.right!=null)
+            stack.push(temp.right);
+            if(temp.left!=null)
+            stack.push(temp.left);
+        }
+        root = q.poll();
+        root.left = null; // First take the root and make left as null.
+        TreeNode temp1 = root;
+        while(!q.isEmpty()){
+            TreeNode k = q.poll(); // Next node
+            k.left = null; // Make the node left is null
+            temp1.right = k; // Assign to root right side
+            temp1 = k; // jumping to next node
+        }
+    }
+}
