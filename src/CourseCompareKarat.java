b0d68c0e6985ae41cc2216c4e398ae956cda38a1
raviteja45 <raviteja.gitam@gmail.com>
2022-08-15 17:58:06 -0500

    Update CourseCompareKarat.java

--- src/CourseCompareKarat.java
+++ src/CourseCompareKarat.java
@@ -82,3 +82,38 @@ public class Karat {
 	}
 //[58,25:[Economics], 58,17:[Software Design,Linear Algebra], 58,94:[Economics], 25,17:[ ], 25,94:[Economics], 17,94:[ ]]
 }
+
+/**
+You are a developer for a university. Your current project is to develop a system for students to find courses they share with friends. The university has a system for querying courses students are enrolled in, returned as a list of (ID, course) pairs.
+
+Write a function that takes in a collection of (student ID number, course name) pairs and returns, for every pair of students, a collection of all courses they share.
+
+
+Sample Input:
+
+enrollments1 = {
+  "58": "Linear Algebra",
+  "94": "Art History",
+  "94": "Operating Systems",
+  "17": "Software Design",
+  "58": "Mechanics",
+  "58": "Economics",
+  "17": "Linear Algebra",
+  "17": "Political Science",
+  "94": "Economics",
+  "25": "Economics",
+  "58": "Software Design"
+}
+
+Sample Output (pseudocode, in any order):
+
+find_pairs(enrollments1) =>
+{
+  "58,17": ["Software Design", "Linear Algebra"]
+  "58,94": ["Economics"]
+  "58,25": ["Economics"]
+  "94,25": ["Economics"]
+  "17,94": []
+  "17,25": []
+}
+*/
