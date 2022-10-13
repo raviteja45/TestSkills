bd45a9ef40d09a945125f42f729ff3289296d06d
raviteja45 <raviteja.gitam@gmail.com>
2022-10-13 16:10:22 -0500

    Update 1884_Egg_Drop_With_2_Eggs_and_N_Floors.java

--- 1884_Egg_Drop_With_2_Eggs_and_N_Floors.java
+++ 1884_Egg_Drop_With_2_Eggs_and_N_Floors.java
@@ -23,6 +23,21 @@ class Solution {
     }
 }
 
+
+class Solution { // With K eggs, N floors
+    public int twoEggDrop(int n) {
+        int[][] dp = new int[n+1][3];
+        int m = 0;
+        while(dp[m][2]<n){
+            m++;
+            for(int i=1;i<=2;i++){
+                dp[m][i] = dp[m-1][i-1]+dp[m-1][i]+1;
+            }
+        }
+        return m;
+    }
+}
+
 /**
 You are given two identical eggs and you have access to a building with n floors labeled from 1 to n.
 
