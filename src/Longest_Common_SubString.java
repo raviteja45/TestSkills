1bd3cfe8b4177f0aedb075bf2d31a81f4d8a6712
raviteja45 <raviteja.gitam@gmail.com>
2022-10-31 13:42:32 -0500

    Update Longest_Common_SubString.java

--- src/Longest_Common_SubString.java
+++ src/Longest_Common_SubString.java
@@ -12,6 +12,23 @@ public int LCS(String input1, String input2) {
 			return result;
 		}
 
+----------------------V2 version-------------------------------------
+	
+public int LCS(String input1, String input2) {
+		int result = 0;
+		int[][] dp = new int[input1.length() + 1][input2.length() + 1];
+		for (int i = 1; i <= input1.length(); i++) {
+			for (int j = 1; j <= input2.length(); j++) {
+				if (input1.charAt(i - 1) == input2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Integer.max(dp[i][j-1],dp[i-1][j]);
+				}
+			}
+		}
+		return dp[input1.length()][input2.length()];
+	}
+
 /**
 System.out.println(t1.LCS("abcd", "xabc")); //3
 SubString - should be in sequence
