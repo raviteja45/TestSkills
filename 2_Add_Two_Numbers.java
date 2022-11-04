945bd954c3852b6dc7fa15875e04a5bc844f169d
raviteja45 <raviteja.gitam@gmail.com>
2022-11-04 12:48:50 -0500

    Update 2_Add_Two_Numbers.java

--- 2_Add_Two_Numbers.java
+++ 2_Add_Two_Numbers.java
@@ -30,6 +30,51 @@ class Solution {
     }
 }
 
+------------------------------V2---------------------------------------
+    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        while(l1!=null){
+            q1.add(l1.val);
+            l1 = l1.next;
+        }
+        while(l2!=null){
+            q2.add(l2.val);
+            l2 = l2.next;
+        }
+        ListNode res = new ListNode();
+        ListNode head = res;
+        int carry = 0;
+        while(!q1.isEmpty()||!q2.isEmpty()){
+            int temp1 = q1.isEmpty()?0:q1.poll();
+            int temp2 = q2.isEmpty()?0:q2.poll();
+            int fin = temp1+temp2+carry;
+            ListNode listNode = new ListNode(fin%10);
+            carry = fin/10;
+            res.next = listNode;
+            res = listNode;
+        }
+        if(carry>0){
+            ListNode temp = new ListNode(carry);
+            res.next = temp;
+            res  = temp;
+        }
+        return head.next;
+    }
+}
+
 /**
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
