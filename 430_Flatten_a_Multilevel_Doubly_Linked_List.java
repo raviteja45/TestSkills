9c6a711cf8a220bb0c8bb6040aab40f9ee57aa73
raviteja45 <raviteja.gitam@gmail.com>
2023-02-18 22:54:46 +0530

    Update 430_Flatten_a_Multilevel_Doubly_Linked_List.java

--- 430_Flatten_a_Multilevel_Doubly_Linked_List.java
+++ 430_Flatten_a_Multilevel_Doubly_Linked_List.java
@@ -34,6 +34,29 @@ class Solution {
     }
 }
 
+-------------------------V2---------------------------------
+    
+    class Solution {
+    public Node flatten(Node head) {
+        Stack<Node> stack = new Stack<>();
+        Node current = head;
+        while(head!=null){
+            if(head.child!=null){
+                if(head.next!=null){
+                    stack.push(head.next);
+                }
+                head.next = head.child;
+                head.next.prev = head;
+                head.child = null;
+            }else if(head.next==null&&!stack.isEmpty()){ // reach of tail of child,  reconnet the next of parent
+                head.next = stack.pop();
+                head.next.prev = head;
+            }
+            head = head.next;
+        }
+        return current;
+    }
+}
 
 /**
 You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer, and an additional child pointer. This child pointer may or may not point to a separate doubly linked list, also containing these special nodes. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure as shown in the example below.
